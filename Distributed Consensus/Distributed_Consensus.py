9ea9413d3c020a212007c524f57cf2a5b7b10c11
CGFrog <iancoop25@gmail.com>
2024-12-28 02:15:50 -0600

    You can now evaluate the convergence patterns of many graphs instead of just one.

--- Distributed Consensus/Distributed_Consensus.py
+++ Distributed Consensus/Distributed_Consensus.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.random import rand
 
 def update_values(G):
     new_values = {}
@@ -8,10 +9,42 @@ def update_values(G):
         neighbors = list(G.neighbors(node))
         values = [G.nodes[n]['value'] for n in neighbors]
         values.append(G.nodes[node]['value'])
-        new_values[node] = np.mean(values) if values else G.nodes[node]['value']
-    
+        new_values[node] = np.mean(values)
     for node, value in new_values.items():
         G.nodes[node]['value'] = value
+
+def update_weighted_values(G):
+    w_neighbor = 1
+    w_agent = 1
+    new_values = {}
+    #Arithmetic Weighted average = sum(w_ix_i)/sum(w_i)
+    for node in G.nodes:
+        neighbors = list(G.neighbors(node))
+        values = [G.nodes[n]['value'] for n in neighbors]
+        total = w_neighbor*sum(values) + (w_agent * G.nodes[node]['value'])
+        new_values[node] = total/((len(values) * w_neighbor) + w_agent) 
+    #After all new values have been calculated replace each agents current value with the new average.
+    for node, value in new_values.items():
+        G.nodes[node]['value'] = value
+        
+def has_converged(G, global_average):
+    for node in G.nodes:
+        if abs(G.nodes[node]['value'] - global_average) > 0.001:
+            return False
+    return True
+
+def has_converged_islands(G):
+    connected_components = nx.connected_components(G)
+    
+    for component in connected_components:
+        nodes_in_component = list(component)
+        local_average = sum(G.nodes[node]['value'] for node in nodes_in_component) / len(nodes_in_component)
+        
+        for node in nodes_in_component:
+            if abs(G.nodes[node]['value'] - local_average) > 0.001:
+                return False
+    
+    return True
 
 def display_final_graph(G):
     values = [G.nodes[node]['value'] for node in G.nodes]
@@ -24,14 +57,14 @@ def display_final_graph(G):
     nx.draw_networkx_labels(G, pos, labels=labels, font_size=10, font_color="white")
     
     plt.colorbar(plt.cm.ScalarMappable(cmap=plt.cm.viridis), label="Node Values")
-    plt.title("Final State of the Graph with Node Labels and Values")
+    plt.title("Graph")
     plt.show()
 
 def global_average(G):
     values = [G.nodes[n]['value'] for n in G.nodes]
     return np.mean(values)
 
-def track_values_and_averages(G, iterations):
+def track_values_and_averages(G):
     node_values_over_time = {node: [] for node in G.nodes}
     global_averages = []
     
@@ -39,13 +72,25 @@ def track_values_and_averages(G, iterations):
         node_values_over_time[node].append(G.nodes[node]['value'])
     global_averages.append(global_average(G))
     
-    for _ in range(iterations):
-        update_values(G)
-        
-        for node in G.nodes:
-            node_values_over_time[node].append(G.nodes[node]['value'])
-        global_averages.append(global_average(G))
+    i = 0
     
+    if nx.nx.is_connected(G):
+        while not has_converged(G, global_averages[i]):
+            update_weighted_values(G)
+            i+=1
+            for node in G.nodes:
+                node_values_over_time[node].append(G.nodes[node]['value'])
+            global_averages.append(global_average(G))
+    # Try to rewrite this to reduce code duplication. 
+    else:
+        while not has_converged_islands(G):
+            update_weighted_values(G)
+            i+=1
+            for node in G.nodes:
+                node_values_over_time[node].append(G.nodes[node]['value'])
+            global_averages.append(global_average(G))
+
+
     return node_values_over_time, global_averages
 
 def plot_values_and_global_average(node_values_over_time, global_averages):
@@ -58,7 +103,7 @@ def plot_values_and_global_average(node_values_over_time, global_averages):
     
     plt.xlabel('Iteration')
     plt.ylabel('Value')
-    plt.title('Node Values and Global Average Over Iterations')
+    plt.title('Node Values/Global Average')
     plt.legend()
     plt.grid(True)
     plt.show()
@@ -68,7 +113,7 @@ def set_rand_node_values(G):
     node_mapping = {node: i for i, node in enumerate(G.nodes)}
     nx.set_node_attributes(G, {node: {'value': initial_values[node_mapping[node]]} for node in G.nodes})
 
-def main():
+def custom_graph():
     graph_type = input("Select graph type (binomial, cyclic, k-regular): ")
 
     if graph_type == "binomial":
@@ -86,13 +131,14 @@ def main():
         print("Invalid graph type. Exiting...")
         return
 
+
+
     set_rand_node_values(G)
 
     initial_average = global_average(G)
     print(f"Initial global average: {initial_average:.4f}")
     
-    iterations = int(input("Enter number of iterations to simulate consensus: "))
-    node_values_over_time, global_averages = track_values_and_averages(G, iterations)
+    node_values_over_time, global_averages = track_values_and_averages(G)
 
     final_average = global_average(G)
     print(f"Final global average: {final_average:.4f}")
@@ -102,4 +148,167 @@ def main():
     print("Consensus process complete!")
 
 
-main()
+'''
+New Goal:
+
+    - Plot convergence of multiple graphs 
+    - x axis corresponding iteration to converge 
+    - y axis corresponding to some independent variable.
+
+    
+    Step 1: Convergence detection
+        We must determine when a graph has succesfully converged.
+        We must define an error range for our convergence, i.e, when all nodes are within .05% of global average
+        If there are islands we must discard the graph since convergence is impossible.
+'''
+
+def independent_variable_selection(graph):
+    if graph == "binomial":
+        ind_var = input("Select independent variable type (order, probability)")
+        return ind_var
+    if graph == "k-regular":
+        ind_var = input("Select independent variable type (order, degree)")
+        return ind_var
+    else:
+        print ("Cyclic chosen, independent variable set to order.")
+        return "order"
+
+def have_islands():
+    allow_islands = input("Allow graphs to contain islands? (Type Y for yes, or press any other key for no)")
+    if allow_islands == 'Y':
+        allow_islands = True
+    else:
+        allow_islands = False
+    return allow_islands
+
+def convergence_point(G, allow_islands):
+    iterations = 0
+    g_avg = global_average(G)
+    if not allow_islands:
+        while not has_converged(G, g_avg):
+            iterations += 1
+            update_weighted_values(G)
+    
+    elif allow_islands:
+        while not has_converged_islands(G):
+            iterations += 1
+            update_weighted_values(G)
+    return iterations
+
+
+# Definetly should use classes for this instead 
+def binomial_sim(ind_var, allow_islands, iterations, data_points, ind_var_data):
+    if ind_var == "order":
+        n = int(input("Enter number of nodes: "))
+        for d in range(data_points):
+            print (d)
+            rp = np.random.rand()
+            G = nx.erdos_renyi_graph(n,rp)
+            set_rand_node_values(G)
+            if allow_islands == False and nx.is_connected(G) == False:
+                d-1
+                continue
+            iterations.append(convergence_point(G,allow_islands))
+            ind_var_data.append(rp)
+    else:
+        p = float(input("Enter edge probability [0,1]: "))
+        for d in range(data_points):
+            print (d)
+            rn = np.random.randint(1,100)
+            G = nx.erdos_renyi_graph(rn, p)
+            set_rand_node_values(G)
+            if allow_islands == False and nx.is_connected(G) == False:
+                d-1
+                continue
+            iterations.append(convergence_point(G,allow_islands))
+            ind_var_data.append(rn)
+
+def kreg_sim(ind_var, allow_islands, iterations, data_points, ind_var_data):
+        if ind_var == "order":
+            n = int(input("Enter number of nodes: "))
+            for d in range(data_points):
+                print (d)
+                rn = np.random.randint(1,n)
+                G = nx.random_regular_graph(rn,n)
+                set_rand_node_values(G)
+                if allow_islands == False and nx.is_connected(G) == False:
+                    d-1
+                    continue
+                iterations.append(convergence_point(G,allow_islands))
+                ind_var_data.append(rn)
+        else:
+            k = int(input("Enter degree of nodes for: "))
+            for d in range(data_points):
+                print (d)
+                rk = np.random.randint(k,k+100)
+                if k*rk%2 == 1:
+                    d-=1
+                    continue
+                G = nx.random_regular_graph(k,rk)
+                set_rand_node_values(G)
+                if allow_islands == False and nx.is_connected(G) == False:
+                    d-1
+                    continue
+                iterations.append(convergence_point(G,allow_islands))
+                ind_var_data.append(rk)
+
+def cyclic_sim(ind_var, allow_islands, iterations, data_points, ind_var_data):
+    for d in range(data_points):
+        print (d)
+        rn = np.random.randint(1,200)
+        G = nx.cycle_graph(rn)
+        set_rand_node_values(G)
+        if allow_islands == False and nx.is_connected(G) == False:
+            d-1
+            continue
+        iterations.append(convergence_point(G,allow_islands))
+        ind_var_data.append(rn)
+
+def plot_iterations_vs_data(iterations, ind_var_data, title="Iterations vs Data", xlabel="Independent Variable Data", ylabel="Iterations"):
+    plt.figure(figsize=(8, 5))
+    # Plot independent variable data (x-axis) vs. iterations (y-axis)
+    plt.plot(ind_var_data, iterations, marker='o', linestyle='', color='b')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(True)
+    plt.show()
+
+
+def run_sim(graph_type, ind_var, allow_islands, data_points):
+    ind_var_data = []
+    iterations = []
+    if graph_type == "binomial":
+        binomial_sim(ind_var, allow_islands, iterations, data_points, ind_var_data)
+    elif graph_type == "k-regular":        
+        kreg_sim(ind_var, allow_islands, iterations, data_points, ind_var_data)
+    else:
+        cyclic_sim(ind_var, allow_islands, iterations, data_points, ind_var_data)
+    
+    print(f"Length of ind_var_data: {len(ind_var_data)}")
+    print(f"Length of iterations: {len(iterations)}")
+
+    plot_iterations_vs_data(
+        iterations, 
+        ind_var_data, 
+        title=f"{graph_type.capitalize()} Simulation Results",
+        xlabel="Independent Variable Data", 
+        ylabel="Iterations"
+    )
+
+
+def init_sim():
+    graph_type = input("Select graph type (binomial, cyclic, k-regular): ")
+    ind_var = independent_variable_selection(graph_type)
+    allow_islands = have_islands()
+    data_points = eval(input("How many data points to evaluate?"))
+    
+
+
+    run_sim(graph_type, ind_var, allow_islands, data_points)
+    
+
+
+
+
+init_sim()
