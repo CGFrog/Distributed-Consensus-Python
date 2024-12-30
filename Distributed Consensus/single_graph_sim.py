301834cfa53ff143bf9b3f8746cc9dec2a649471
CGFrog <iancoop25@gmail.com>
2024-12-29 19:59:35 -0600

    Single graph simulations have been cleaned up and refactored using OOP design principles.

--- Distributed Consensus/single_graph_sim.py
+++ Distributed Consensus/single_graph_sim.py
@@ -3,138 +3,149 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.random import rand
 
-def update_weighted_values(G):
-    w_neighbor = 1
-    w_agent = 1
-    new_values = {}
-    #Arithmetic Weighted average = sum(w_ix_i)/sum(w_i)
-    for node in G.nodes:
-        neighbors = list(G.neighbors(node))
-        values = [G.nodes[n]['value'] for n in neighbors]
-        total = w_neighbor*sum(values) + (w_agent * G.nodes[node]['value'])
-        new_values[node] = total/((len(values) * w_neighbor) + w_agent) 
-    #After all new values have been calculated replace each agents current value with the new average.
-    for node, value in new_values.items():
-        G.nodes[node]['value'] = value
+class Simulation:
+    def __init__(self,order):
+        self.order = order
+        self.G = nx.empty_graph()
+        self.CONSENSUS_ERROR = 0.001
         
-def has_converged(G, global_average):
-    for node in G.nodes:
-        if abs(G.nodes[node]['value'] - global_average) > 0.001:
-            return False
-    return True
+    def set_rand_node_values(self):
+        initial_values = np.random.rand(len(self.G.nodes))
+        node_mapping = {node: i for i, node in enumerate(self.G.nodes)}
+        nx.set_node_attributes(self.G, {node: {'value': initial_values[node_mapping[node]]} for node in self.G.nodes})
 
-def has_converged_islands(G):
-    connected_components = nx.connected_components(G)
-    
-    for component in connected_components:
-        nodes_in_component = list(component)
-        local_average = sum(G.nodes[node]['value'] for node in nodes_in_component) / len(nodes_in_component)
-        
-        for node in nodes_in_component:
-            if abs(G.nodes[node]['value'] - local_average) > 0.001:
+    def calculate_global_average(self):
+        values = [self.G.nodes[n]['value'] for n in self.G.nodes]
+        return np.mean(values)
+
+    def has_converged(self):
+        gbl_avg = self.calculate_global_average()
+        for node in self.G.nodes:
+            if abs(self.G.nodes[node]['value'] - gbl_avg) > self.CONSENSUS_ERROR:
                 return False
+        return True
     
-    return True
-
-def display_final_graph(G):
-    values = [G.nodes[node]['value'] for node in G.nodes]
-    pos = nx.spring_layout(G)
+    def display_final_graph(self):
+        values = [self.G.nodes[node]['value'] for node in self.G.nodes]
+        pos = nx.spring_layout(self.G)
     
-    plt.figure(figsize=(10, 7))
-    nx.draw(G, pos, with_labels=False, node_color=values, cmap=plt.cm.viridis, node_size=700)
+        plt.figure(figsize=(10, 7))
+        nx.draw(self.G, pos, with_labels=False, node_color=values, cmap=plt.cm.viridis, node_size=700)
     
-    labels = {node: f"{node}\n{G.nodes[node]['value']:.2f}" for node in G.nodes}
-    nx.draw_networkx_labels(G, pos, labels=labels, font_size=10, font_color="white")
+        labels = {node: f"{node}\n{self.G.nodes[node]['value']:.2f}" for node in self.G.nodes}
+        nx.draw_networkx_labels(self.G, pos, labels=labels, font_size=10, font_color="white")
     
-    plt.colorbar(plt.cm.ScalarMappable(cmap=plt.cm.viridis), label="Node Values")
-    plt.title("Graph")
-    plt.show()
+        plt.colorbar(plt.cm.ScalarMappable(cmap=plt.cm.viridis), label="Node Values")
+        plt.title("Graph")
+        plt.show()
 
-def global_average(G):
-    values = [G.nodes[n]['value'] for n in G.nodes]
-    return np.mean(values)
 
-def track_values_and_averages(G):
-    node_values_over_time = {node: [] for node in G.nodes}
-    global_averages = []
+    def plot_values_and_global_average(self,node_values_over_time, global_averages):
+        plt.figure(figsize=(12, 8))
     
-    for node in G.nodes:
-        node_values_over_time[node].append(G.nodes[node]['value'])
-    global_averages.append(global_average(G))
+        for node, values in node_values_over_time.items():
+            plt.plot(values, label=f'Node {node}', linestyle='--', alpha=0.7)
+
+        plt.plot(global_averages, label='Global Average', color='red', linewidth=2, marker='o')
     
-    i = 0
+        plt.xlabel('Iteration')
+        plt.ylabel('Value')
+        plt.title('Node Values/Global Average')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    def has_converged_islands(self):
+        connected_components = nx.connected_components(self.G)
     
-    if nx.nx.is_connected(G):
-        while not has_converged(G, global_averages[i]):
-            update_weighted_values(G)
-            i+=1
-            for node in G.nodes:
-                node_values_over_time[node].append(G.nodes[node]['value'])
-            global_averages.append(global_average(G))
-    # Try to rewrite this to reduce code duplication. 
-    else:
-        while not has_converged_islands(G):
-            update_weighted_values(G)
-            i+=1
-            for node in G.nodes:
-                node_values_over_time[node].append(G.nodes[node]['value'])
-            global_averages.append(global_average(G))
-
-
-    return node_values_over_time, global_averages
-
-def plot_values_and_global_average(node_values_over_time, global_averages):
-    plt.figure(figsize=(12, 8))
+        for component in connected_components:
+            nodes_in_component = list(component)
+            local_average = sum(self.G.nodes[node]['value'] for node in nodes_in_component) / len(nodes_in_component)
+        
+            for node in nodes_in_component:
+                if abs(self.G.nodes[node]['value'] - local_average) > self.CONSENSUS_ERROR:
+                    return False
     
-    for node, values in node_values_over_time.items():
-        plt.plot(values, label=f'Node {node}', linestyle='--', alpha=0.7)
+        return True
 
-    plt.plot(global_averages, label='Global Average', color='red', linewidth=2, marker='o')
+    def track_values_and_averages(self):
+        node_values_over_time = {node: [] for node in self.G.nodes}
+        global_averages = []
     
-    plt.xlabel('Iteration')
-    plt.ylabel('Value')
-    plt.title('Node Values/Global Average')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-def set_rand_node_values(G):
-    initial_values = np.random.rand(len(G.nodes))
-    node_mapping = {node: i for i, node in enumerate(G.nodes)}
-    nx.set_node_attributes(G, {node: {'value': initial_values[node_mapping[node]]} for node in G.nodes})
-
-def custom_graph():
-    graph_type = input("Select graph type (binomial, cyclic, k-regular): ")
-
-    if graph_type == "binomial":
-        n = int(input("Enter number of nodes: "))
-        p = float(input("Enter edge probability [0,1]: "))
-        G = nx.erdos_renyi_graph(n, p)
-    elif graph_type == "cyclic":
-        n = int(input("Enter number of nodes: "))
-        G = nx.cycle_graph(n)
-    elif graph_type == "k-regular":
-        n = int(input("Enter number of nodes for: "))
-        k = int(input("Enter degree of nodes for: "))
-        G = nx.random_regular_graph(k,n)
-    else:
-        print("Invalid graph type. Exiting...")
-        return
-
-    set_rand_node_values(G)
-
-    initial_average = global_average(G)
-    print(f"Initial global average: {initial_average:.4f}")
+        for node in self.G.nodes:
+            node_values_over_time[node].append(self.G.nodes[node]['value'])
+        global_averages.append(self.calculate_global_average())
     
-    node_values_over_time, global_averages = track_values_and_averages(G)
+        i = 0
+    
+        if nx.nx.is_connected(self.G):
+            while not self.has_converged():
+                self.update_weighted_values()
+                i+=1
+                for node in self.G.nodes:
+                    node_values_over_time[node].append(self.G.nodes[node]['value'])
+                global_averages.append(self.calculate_global_average())
+        # Try to rewrite this to reduce code duplication. 
+        else:
+            while not self.has_converged_islands():
+                self.update_weighted_values(self.G)
+                i+=1
+                for node in self.G.nodes:
+                    node_values_over_time[node].append(self.G.nodes[node]['value'])
+                global_averages.append(self.calculate_global_average())
 
-    final_average = global_average(G)
-    print(f"Final global average: {final_average:.4f}")
 
-    plot_values_and_global_average(node_values_over_time, global_averages)
-    display_final_graph(G)
-    print("Consensus process complete!")
+        return node_values_over_time, global_averages
 
+    def update_weighted_values(self):
+        w_neighbor = 1
+        w_agent = 1
+        new_values = {}
+        #Arithmetic Weighted average = sum(w_ix_i)/sum(w_i)
+        for node in self.G.nodes:
+            neighbors = list(self.G.neighbors(node))
+            values = [self.G.nodes[n]['value'] for n in neighbors]
+            total = w_neighbor*sum(values) + (w_agent * self.G.nodes[node]['value'])
+            new_values[node] = total/((len(values) * w_neighbor) + w_agent) 
+        #After all new values have been calculated replace each agents current value with the new average.
+        for node, value in new_values.items():
+            self.G.nodes[node]['value'] = value
+            
+    def run_sim(self):
+        self.set_rand_node_values()
+        self.global_average = self.calculate_global_average()
+        print(f"Initial global average: {self.global_average:.4f}")
+    
+        node_values_over_time, global_averages = self.track_values_and_averages()
 
+        final_average = self.calculate_global_average()
+        print(f"Final global average: {final_average:.4f}")
 
-custom_graph()
+        self.plot_values_and_global_average(node_values_over_time, global_averages)
+        self.display_final_graph()
+        print("Consensus process complete!")
+    
+class Cyclic(Simulation):
+    def __init__(self,order):
+        super().__init__(order)
+        self.G=nx.cycle_graph(order)
+        self.run_sim()
+        
+
+class Kregular(Simulation):
+    def __init__(self,order,degree):
+        super().__init__(order)
+        self.degree=degree
+        self.G=nx.random_regular_graph(degree,order)
+        self.run_sim()
+        
+class Binomial(Simulation):
+    def __init__(self,order,probability):
+        super().__init__(order)
+        self.probability = probability
+        self.G=nx.erdos_renyi_graph(order,probability)
+        self.run_sim()
+        
+#Cyclic(10)
+#Kregular(20,3)
+#Binomial(20,.2)        
